3a33a6ce6ba14e2c01d817d7274f269ac12acd7b
weileizong <493258365@qq.com>
2018-08-15 13:52:46 +0800

    mytest

---
+++ src/main/java/com/raytest/myTestJava.java
@@ -0,0 +1,10 @@
+package com.raytest;
+
+/**
+ * Created by weileizong on 18/8/15.
+ */
+public class myTestJava {
+    public static void main(String args[]){
+        System.out.println("123");
+    }
+}
