9856dd1aacc2f870bedcda47c509b681ddeab751
weileizong <493258365@qq.com>
2018-08-15 15:11:53 +0800

    2222

--- src/main/java/com/raytest/myttt.java
+++ src/main/java/com/raytest/myttt.java
@@ -4,5 +4,9 @@ package com.raytest;
  * Created by weileizong on 18/8/15.
  */
 public class myttt {
-    //12312312
+    //12312312111
+    public static void main(String[] args){
+        System.out.println("123sdsdsdsdss");
+    }
+
 }
